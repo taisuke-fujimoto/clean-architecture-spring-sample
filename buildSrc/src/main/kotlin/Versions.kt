1f177b8d6b20e397de15ee9ecb69522ec6b1aa13
taisuke-fujimoto <taisuke.fujimoto@phoneappli.net>
2021-09-06 10:59:44 +0900

    update dependency

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 // JetBrains
-const val KOTLIN_VERSION = "1.5.21"
+const val KOTLIN_VERSION = "1.5.30"
 const val EXPOSED_VERSION = "0.33.1"
 
 // ktlint
 const val GRADLE_KTLINT_VERSION = "10.1.0"
 
 // spring
-const val SPRING_BOOT_VERSION = "2.5.3"
+const val SPRING_BOOT_VERSION = "2.5.4"
 const val SPRING_DEPENDENCY_MANAGEMENT_VERSION = "1.0.11.RELEASE"
 
 // other
